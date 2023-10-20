91065a916d5653899a8526d856ea3e291c4d6110
Keneel <tkeneel@gmail.com>
2023-10-20 16:14:51 -0500

    X and O can be added to the squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,24 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function(){
+    let player = 'X';
+    let gameState = [];
+    console.log(gameState);
 
     const squares = document.querySelectorAll("#board > div");
     console.log(squares);
 
     squares.forEach(function(elem){
-        elem.classList.add("square")
+        elem.classList.add("square");
+        elem.addEventListener('click', function() {
+            if(!elem.textContent){
+                elem.textContent = player;
+                elem.classList.add(player);
+
+                player = player === "X" ? "O" : "X";  
+                console.log(player);
+            }
+        });
     });
 });
+
