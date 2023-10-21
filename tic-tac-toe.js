0d142a5b941a69362696b84ab74320168dbaf5a4
Keneel <tkeneel@gmail.com>
2023-10-21 10:41:00 -0500

    The game can decided a winner and when their  is a tie

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,21 +2,49 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     let player = 'X';
-    let gameState = [];
-    console.log(gameState);
+    let winner = false;
+    let gameState = ['', '', '', '', '', '', '', '', ''];
+    const gameStatus = document.querySelector("#status")
+
+
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
 
     const squares = document.querySelectorAll("#board > div");
     console.log(squares);
 
-    squares.forEach(function(elem){
+    squares.forEach(function(elem, index){
         elem.classList.add("square");
         elem.addEventListener('click', function() {
             if(!elem.textContent){
                 elem.textContent = player;
                 elem.classList.add(player);
 
-                player = player === "X" ? "O" : "X";  
-                console.log(player);
+
+                gameState[index] = elem.textContent;
+                console.log(gameState);
+
+                const winner = checkWinner();
+                if (winner) {
+                    gameStatus.textContent = `Congratulations! ${player} is the Winner!`;
+                    gameStatus.classList.add("you-won");
+                } else {
+                    player = player === "X" ? "O" : "X";
+                    console.log(player);
+                        if(gameState.every((element) => element !== null && element !== '')){
+                            gameStatus.textContent = `The game is a Tie, Click new game to play again`;
+                        }
+                }
+                
             }
         });
 
@@ -28,5 +56,18 @@ document.addEventListener("DOMContentLoaded", function(){
             elem.classList.remove("hover")
         });
     });
+
+    
+    function checkWinner() {
+        for (let i = 0; i < winningCombinations.length; i++) {
+            const [a, b, c] = winningCombinations[i];
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 });
 
